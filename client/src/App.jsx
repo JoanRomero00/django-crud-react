df30eceb0b125845db2ecf393acd7396969edd7d
Joan Romero <joanromerosfc@gmail.com>
2024-12-11 23:40:22 -0300

    Add NavVar and Login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,23 @@ import { Navigation } from "./components/Navigation";
 import { TaskFormPage } from "./pages/TaskFormPage";
 import { TasksPage } from "./pages/TasksPage";
 import { Toaster } from "react-hot-toast";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
-    <div class="container mx-auto">
       <BrowserRouter>
         <Navigation />
-        <Routes>
-          <Route path="/" element={<Navigate to="/tasks" />} />
-          <Route path="/tasks" element={<TasksPage />} />
-          <Route path="/tasks-create" element={<TaskFormPage />} />
-          <Route path="/tasks/:id" element={<TaskFormPage />} />
-        </Routes>
-        <Toaster />
+        <div className="container mx-auto">
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/tasks" element={<TasksPage />} />
+            <Route path="/tasks-create" element={<TaskFormPage />} />
+            <Route path="/tasks/:id" element={<TaskFormPage />} />
+          </Routes>
+          <Toaster />
+        </div>
       </BrowserRouter>
-    </div>
   );
 }
 
